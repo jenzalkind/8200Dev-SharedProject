3fbb60f50d04fe5d7032f8b3ea5c2a599c91d6a5
FuryaNaor <furyanaor@gmail.com>
2022-08-30 17:40:48 +0300

    ttest commit'

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const split = require('./split.js');
+const fs = require('fs');
+
+app.get('/', function(req, res){
+	fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+})
+
+app.get('/split/my_split/:number', function(req, res){
+    res.status(200).send(split.my_split(req.params.number));
+})
+
+exports.stop = function(){
+    server.close();
+}
+const server = app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
+});
